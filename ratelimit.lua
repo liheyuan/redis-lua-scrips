6dcc0a7bb95193a2100fc8da3e0d8fad7db3bf7d
lihy <lihy@fenbi.com>
2021-01-09 17:35:50 +0800

    FIX: ratelimit lua

--- ratelimit.lua
+++ ratelimit.lua
@@ -1,12 +1,12 @@
 local key = KEYS[1]
 local limit = tonumber(ARGV[1])
 
-local curLimit = tonumber(redis.call('get', key) or "0")
+local curLimit = tonumber(redis.call('get', key) or '0')
 
 if curLimit + 1 > limit then
     return 0
 else
-    redis.call("incrby", key, 1)
-    redis.call("expire", key, 1)
+    redis.call('incrby', key, 1)
+    redis.call('expire', key, 1)
     return 1
 end
