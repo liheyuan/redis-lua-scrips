4caad9c3483b1c2ac87a4654d1d2e82cf489426c
lihy <lihy@fenbi.com>
2021-01-19 22:15:46 +0800

    FIX: typo

--- dislock.lua
+++ dislock.lua
@@ -2,7 +2,7 @@ local key = KEYS[1]
 local idfy = ARGV[1]
 local timeout_secs = tonumber(ARGV[2])
 
-if redis.call('set', key, idfy, "NX", "EX", timeout_secs) then
+if redis.call('set', key, idfy, 'NX', 'EX', timeout_secs) then
     return 1
 else
     return 0
