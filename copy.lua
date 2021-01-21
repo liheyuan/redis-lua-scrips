23cd98eec5d0d89b115388a8ee254a38c8eeb103
lihy <lihy@fenbi.com>
2021-01-21 16:57:47 +0800

    FIX: check

--- copy.lua
+++ copy.lua
@@ -1,6 +1,10 @@
 local srcKey = KEYS[1]
 local dstKey = KEYS[2]
 
+if redis.call('exists', srcKey) == 0 then
+    return 0
+end
+
 if redis.call('exists', dstKey) == 1 then
     redis.call('del', dstKey)
 end
