48b6a698681efa78a3eba44b9909f9bba7a1eee3
lihy <lihy@fenbi.com>
2021-01-21 21:12:27 +0800

    FIX: seincrby

--- seincrby.lua
+++ seincrby.lua
@@ -2,17 +2,18 @@ local key = KEYS[1];
 local delta = tonumber(ARGV[1]);
 local sentryVal = tonumber(ARGV[2]);
 
-local val = redis.call('get', key);
-local newVal = val + delta
+local val = tonumber(redis.call('get', key)) or 0;
+
+local newVal = val + delta;
 if val then
     if delta > 0 and newVal > sentryVal then
-        return -20000;
+        return nil;
     end;
     if delta < 0 and newVal < sentryVal then
-        return -20000;
+        return nil;
     end;
 else
-    return -10000
+    return nil;
 end;
 
 return redis.call('incrby', key, delta);
